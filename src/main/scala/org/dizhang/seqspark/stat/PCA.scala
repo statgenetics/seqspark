fd85464d64d0284fdc6dc26b902e4a2d96d10b3e
Di Zhang <zhangdi.devel@gmail.com>
2016-10-09 14:10:44 -0500

    testing

--- src/main/scala/org/dizhang/seqspark/stat/PCA.scala
+++ src/main/scala/org/dizhang/seqspark/stat/PCA.scala
@@ -4,7 +4,7 @@ import breeze.linalg.{DenseMatrix => BDM}
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.feature.{PCA => SPCA}
 import org.apache.spark.rdd.RDD
-import org.dizhang.seqspark.ds.Genotype
+import org.dizhang.seqspark.ds.{DenseCounter, Genotype, SparseCounter}
 import org.dizhang.seqspark.worker.Data
 import org.dizhang.seqspark.util.General._
 import org.slf4j.LoggerFactory
@@ -19,8 +19,19 @@ class PCA[A: Genotype](vcf: Data[A]) extends Serializable {
 
   def geno = implicitly[Genotype[A]]
 
-  def transpose: RDD[Vector] = {
-    val input = vcf
+  def prepare: RDD[Vector] = {
+
+    vcf.map{v =>
+      val af = v.toCounter(geno.toAAF, (0.0, 2.0)).reduce.ratio
+      val cnt = v.toCounter(geno.toBRV(_, af), 0.0)
+      cnt match {
+        case DenseCounter(elems, _) => Vectors.dense(elems.toArray)
+        case SparseCounter(elems, _, n) => Vectors.sparse(n, elems.toSeq.sortBy(_._1))
+      }
+    }
+
+    /**
+      * val input = vcf
     val byColAndRow = input.zipWithIndex().flatMap{
       case (v, ri) =>
         val maf = v.toCounter(geno.toAAF, (0.0, 2.0)).reduce.ratio
@@ -32,13 +43,15 @@ class PCA[A: Genotype](vcf: Data[A]) extends Serializable {
     val res = byCol.map {
       row => Vectors.dense(row.toArray.sortBy(_._1).map(_._2))
     }
+
     res.cache()
     logger.info(s"PCA transposed data dimension: ${res.count()} x ${res.first().size}")
     res
+      */
   }
   def pc(n: Int): BDM[Double] = {
     val model = new SPCA(n)
-    val data = this.transpose
+    val data = this.prepare
     val res = model.fit(data).pc.values
     new BDM(res.length/n, n, res)
   }

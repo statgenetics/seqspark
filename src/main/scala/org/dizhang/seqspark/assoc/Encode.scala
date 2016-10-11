aa94da7e121e72f17d124763006ddc4b2f085194
Di Zhang <zhangdi.devel@gmail.com>
2016-10-11 09:24:33 -0500

    testing

--- src/main/scala/org/dizhang/seqspark/assoc/Encode.scala
+++ src/main/scala/org/dizhang/seqspark/assoc/Encode.scala
@@ -312,13 +312,13 @@ abstract class Encode[A: Genotype] extends Serializable {
     val sortedMaf = maf.filter(m => m < fixedCutoff || m > (1 - fixedCutoff))
       .map(m => if (m < 0.5) m else 1 - m).sorted
     if (sortedMaf.isEmpty) {
-      println(s"sortedMaf length should be 0 == ${sortedMaf.length}")
+      //println(s"sortedMaf length should be 0 == ${sortedMaf.length}")
       None
     } else {
-      println(s"sortedMaf length should be 0 < ${sortedMaf.length}")
+      //println(s"sortedMaf length should be 0 < ${sortedMaf.length}")
       Some(sortedMaf.map(c => Array(c)).reduce{(a, b) =>
-        println(s"a: ${a.mkString(",")} b: ${b.mkString(",")}")
-        if (a(-1) + tol >= b(0))
+        //println(s"a: ${a.mkString(",")} b: ${b.mkString(",")}")
+        if (a.last + tol >= b(0))
           a.slice(0, a.length - 1) ++ b
         else
           a ++ b})
---
+++ src/test/scala/org/dizhang/seqspark/assoc/EncodeSpec.scala
@@ -0,0 +1,31 @@
+package org.dizhang.seqspark.assoc
+
+import breeze.stats.distributions.Binomial
+import com.typesafe.config.ConfigFactory
+import org.dizhang.seqspark.ds.Variant
+import org.dizhang.seqspark.util.UserConfig.MethodConfig
+import org.scalatest.FlatSpec
+
+/**
+  * Created by zhangdi on 10/11/16.
+  */
+class EncodeSpec extends FlatSpec {
+
+  val encode = {
+    val conf = ConfigFactory.load().getConfig("seqspark.association.method.vt")
+    val method = MethodConfig(conf)
+    val randg = Binomial(2, 0.02)
+    val vars = (0 to 10).map{i =>
+      val meta = Array("1", i.toString, ".", "A", "C", ".", ".", ".")
+      val geno = randg.sample(1000).map(_.toByte)
+      Variant.fromIndexedSeq(meta, geno, 0.toByte)
+    }
+    Encode(vars, None, None, None, method)
+  }
+
+  "thresholds" should "work" in {
+    val th = encode.thresholds.get
+    //println(th.mkString(","))
+  }
+
+}

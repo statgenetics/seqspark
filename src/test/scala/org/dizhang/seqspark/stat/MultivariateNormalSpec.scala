9ecc4a6f43c1b6599d52043c24c6977f4be73064
Di Zhang <zhangdi.devel@gmail.com>
2016-10-11 12:43:19 -0500

    testing

---
+++ src/test/scala/org/dizhang/seqspark/stat/MultivariateNormalSpec.scala
@@ -0,0 +1,25 @@
+package org.dizhang.seqspark.stat
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.stats.distributions.{Binomial, Gaussian}
+import org.scalatest.FlatSpec
+
+/**
+  * Created by zhangdi on 10/11/16.
+  */
+class MultivariateNormalSpec extends FlatSpec {
+  val obj = {
+    val rand = Binomial(2, 0.02)
+    val dat = (0 to 10).map{i =>
+      rand.sample(2000).map(_.toDouble)
+    }
+    val dm = DenseMatrix(dat: _*)
+    val cov = dm * dm.t
+    MultivariateNormal.Centered(cov)
+  }
+  "A MVN" should "be fine" in {
+    val res = obj.cdf(DenseVector.fill(11)(0.0))
+    println(s"pval: ${res.pvalue} err: ${res.error} info: ${res.inform}")
+  }
+
+}

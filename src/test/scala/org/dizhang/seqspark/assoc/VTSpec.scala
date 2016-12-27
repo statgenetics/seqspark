31855b5596451912e996343651a6c4d28c016ee5
Di Zhang <zhangdi.devel@gmail.com>
2016-12-27 12:14:27 -0600

    test VT analytic

--- src/test/scala/org/dizhang/seqspark/assoc/VTSpec.scala
+++ src/test/scala/org/dizhang/seqspark/assoc/VTSpec.scala
@@ -1,20 +1,22 @@
 package org.dizhang.seqspark.assoc
 
-import breeze.linalg.{DenseMatrix, DenseVector, sum}
+import breeze.linalg.{CSCMatrix, DenseMatrix, DenseVector, SparseVector, sum}
 import breeze.stats.distributions.{Binomial, Gaussian, RandBasis, ThreadLocalRandomGenerator}
 import com.typesafe.config.ConfigFactory
 import org.apache.commons.math3.random.MersenneTwister
 import org.scalatest.FlatSpec
-import org.dizhang.seqspark.ds.{Genotype, Variant}
+import org.dizhang.seqspark.ds.{Genotype, Variant, Variation}
 import org.dizhang.seqspark.stat.{LinearRegression, ScoreTest}
 import org.dizhang.seqspark.util.UserConfig.MethodConfig
 import org.dizhang.seqspark.util.Constant
+import org.slf4j.LoggerFactory
 /**
   * Created by zhangdi on 10/11/16.
   */
 class VTSpec extends FlatSpec {
   val randBasis: RandBasis = new RandBasis(new ThreadLocalRandomGenerator(new MersenneTwister(100)))
 
+  val logger = LoggerFactory.getLogger(getClass)
   val encode = {
     val conf = ConfigFactory.load().getConfig("seqspark.association.method.vt")
     val method = MethodConfig(conf)
@@ -28,6 +30,27 @@ class VTSpec extends FlatSpec {
     val sm = method.config.root().render()
     Encode(vars, sm)
   }
+
+  val nm: SKATO.NullModel = {
+    val file = scala.io.Source.fromURL(getClass.getResource("/2k.tsv")).getLines().toArray
+    val header = file.head.split("\t")
+    val dat = for (s <- file.slice(1, file.length)) yield s.split("\t")
+    val pheno = header.zip(for (i <- header.indices)
+      yield DenseVector(dat.map(x => try {x(i).toDouble} catch {case e: Exception => 0.0}))).toMap
+    val lr = LinearRegression(pheno("bmi"), DenseVector.horzcat(pheno("age"), pheno("sex"), pheno("disease")))
+    SKATO.NullModel(lr)
+  }
+
+  def geno(fn: String): Array[SparseVector[Double]] = {
+    val file = scala.io.Source.fromURL(getClass.getResource(fn)).getLines().toArray
+    val dat = for (s <- file) yield s.split(",")
+    val idxes = for (i <- dat.head.indices) yield dat.map(_(i).toDouble).zipWithIndex.filter(_._1 != 0.0).map(_._2)
+    val arrs = for (i <- dat.head.indices) yield dat.map(_(i).toDouble).filter(_ != 0.0)
+    idxes.zip(arrs).map{p =>
+      new SparseVector[Double](p._1, p._2, dat.length)
+    }.toArray
+  }
+
   val ec2 = {
     val conf = ConfigFactory.load().getConfig("seqspark.association.method.vt")
     val method = MethodConfig(conf)
@@ -52,8 +75,14 @@ class VTSpec extends FlatSpec {
     ScoreTest.NullModel(reg)
   }
   "A VT" should "be fine" in {
-    println(s"defined: ${encode.isDefined} informative: ${encode.informative()} mut: ${sum(encode.getFixed.coding)}")
-    val vt = VT(nullModel, encode.getVT)
-    println(s"S: ${vt.statistic} P: ${vt.pValue}")
+
+    val cln6 = geno("/CLN6").map(_.toDenseVector)
+
+    val vt = VT.AnalyticTest(nm.STNullModel, Encode.VT(cln6, Array[Variation]()))
+
+    logger.info(vt.result.toString)
+    //println(s"defined: ${encode.isDefined} informative: ${encode.informative()} mut: ${sum(encode.getFixed.coding)}")
+    //val vt = VT(nullModel, encode.getVT)
+    //println(s"S: ${vt.statistic} P: ${vt.pValue}")
   }
 }
